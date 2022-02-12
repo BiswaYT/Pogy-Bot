fc99c7dff9a21ad6d304382e9111075885bd604a
Biswa  YT <88150062+BiswaYT@users.noreply.github.com>
2022-02-12 10:18:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+ "verification": "",
+ "description": "Pogy is a multi-purpose discord bot ready to skill up and boost up your Discord server!",
+ "domain": "https://Pogy-1.bqnbiswa.repl.co", // domain
+ "google_analitics": "", // google analitics
+ "token": process.env.TOKEN,
+ "https":"https://", // leave as is
+ "port":"5003",
+
+ "client_id":"892817120847626251", // bot client ID
+ "secret":"6CaatCpwMpUKk3Fm4UQMgee48PLZEljz" // bot client secret for auth
+
+}
+
+
+// for dashboard, read more on https://github.com/IgorKowalczyk/majobot
